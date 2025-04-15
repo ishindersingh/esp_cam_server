7401912830cf4bfb73f0467c30cd2d5eded074ae
Ishinder Singh <78218217+ishindersingh@users.noreply.github.com>
2025-04-16 01:21:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const WebSocket = require('ws');
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ path: '/cam' });
+const wss = new WebSocket.Server({ server, path: '/cam' });
 
 app.get('/', (req, res) => {
   res.send('WebSocket server for ESP32-CAM');
